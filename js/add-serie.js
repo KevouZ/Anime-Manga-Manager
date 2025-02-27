13ca270c1a4724b048a4452135bc28b21c07c992
Kevin Marquez <kmarrufo62@gmail.com>
2025-02-27 16:46:13 -0500

    New styles were added to the content of index.html, the logic of main.js was separated, add-serie.js was created for that purpose, and the .gitignore file was added.

---
+++ js/add-serie.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".content-form");
+const btnAnime = document.querySelector("#addAnime");
+const btnManga = document.querySelector("#addManga");
+
+function addSerie(type){
+    const title = document.querySelector("#nameInput").value.trim();
+    const genre = document.querySelector("#genreInput").value.trim();
+    const status = document.querySelector("input[name='statusInput']:checked").value;
+
+    if (!title || !genre) {
+        alert("Please fill in all the details.");
+        return;
+    }
+
+    const series = JSON.parse(localStorage.getItem(`${type}Data`)) || [];
+    const serieData = { title, genre, status };
+    series.push(serieData);
+
+    localStorage.setItem(`${type}Data`, JSON.stringify(series));
+    form.reset();
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+btnAnime.addEventListener("click", () => addSerie("anime"));
+btnManga.addEventListener("click", () => addSerie("manga"));
--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
-const form = document.querySelector(".content-form");
-const btnAnime = document.querySelector("#addAnime");
-const btnManga = document.querySelector("#addManga");
+const btnStart = document.querySelector("#btn-start");
 
-function addSerie(type){
-    const title = document.querySelector("#nameInput").value.trim();
-    const genre = document.querySelector("#genreInput").value.trim();
-    const status = document.querySelector("input[name='statusInput']:checked").value;
-
-    if (!title || !genre) {
-        alert("Please fill in all the details.");
-        return;
-    }
-
-    const series = JSON.parse(localStorage.getItem(`${type}Data`)) || [];
-    const serieData = { title, genre, status };
-    series.push(serieData);
-
-    localStorage.setItem(`${type}Data`, JSON.stringify(series));
-    form.reset();
-}
+btnStart.addEventListener("click", () => {
+    window.location.href = "add-serie.html";
+})
 
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
-
-btnAnime.addEventListener("click", () => addSerie("anime"));
-btnManga.addEventListener("click", () => addSerie("manga"));
